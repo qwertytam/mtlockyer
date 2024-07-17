f3121060939f71e7875df99fdfa32a34dd331757
Tom Marshall <tom.q.marshall@gmail.com>
2024-07-16 21:41:40 -0400

    deploy debugging

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,5 +1,5 @@
 import { Duration, Stack, StackProps, Tags } from 'aws-cdk-lib';
-import { LambdaIntegration, LambdaRestApi, Period, RateLimitedApiKey } from 'aws-cdk-lib/aws-apigateway';
+// import { LambdaIntegration, LambdaRestApi, Period, RateLimitedApiKey } from 'aws-cdk-lib/aws-apigateway';
 import { DockerImageCode, DockerImageFunction } from 'aws-cdk-lib/aws-lambda';
 import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { Construct } from 'constructs';
@@ -17,7 +17,7 @@ export class InfraStack extends Stack {
 
     const lambdaFunction = new DockerImageFunction(this, `${props.pascalCaseFullName}SeleniumLambda`, {
       code: DockerImageCode.fromImageAsset("../src"),
-      timeout: Duration.seconds(40),
+      timeout: Duration.seconds(90),
       functionName: `${props.fullName}-function`,
       memorySize: 512,
       logRetention: RetentionDays.ONE_WEEK
@@ -25,35 +25,35 @@ export class InfraStack extends Stack {
 
     Tags.of(lambdaFunction).add("Customer", props.applicationTag);
 
-    const gateway = new LambdaRestApi(this, `${props.pascalCaseFullName}Gateway`, {
-      handler: lambdaFunction,
-      proxy: false
-    });
+    // const gateway = new LambdaRestApi(this, `${props.pascalCaseFullName}Gateway`, {
+    //   handler: lambdaFunction,
+    //   proxy: false
+    // });
 
-    Tags.of(gateway).add("Customer", props.applicationTag);
+    // Tags.of(gateway).add("Customer", props.applicationTag);
 
-    const integration = new LambdaIntegration(lambdaFunction);
-    gateway.root.addMethod("GET", integration, {
-      apiKeyRequired: true
-    });
+    // const integration = new LambdaIntegration(lambdaFunction);
+    // gateway.root.addMethod("GET", integration, {
+    //   apiKeyRequired: true
+    // });
 
-    const usagePlan = gateway.addUsagePlan(`${props.pascalCaseFullName}UsagePlan`, {
-      name: `${props.fullName}-usage-plan`,
-      quota: {
-        limit: 100,
-        period: Period.DAY
-      }
-    });
+    // const usagePlan = gateway.addUsagePlan(`${props.pascalCaseFullName}UsagePlan`, {
+    //   name: `${props.fullName}-usage-plan`,
+    //   quota: {
+    //     limit: 100,
+    //     period: Period.DAY
+    //   }
+    // });
 
-    const apiKey = gateway.addApiKey(`${props.pascalCaseFullName}ApiKey`, {
-      apiKeyName: `${props.fullName}-api-key`,
-      value: props.apiKey
-    });
+    // const apiKey = gateway.addApiKey(`${props.pascalCaseFullName}ApiKey`, {
+    //   apiKeyName: `${props.fullName}-api-key`,
+    //   value: props.apiKey
+    // });
 
-    usagePlan.addApiKey(apiKey);
+    // usagePlan.addApiKey(apiKey);
 
-    usagePlan.addApiStage({
-      stage: gateway.deploymentStage,
-    });
+    // usagePlan.addApiStage({
+    //   stage: gateway.deploymentStage,
+    // });
   }
 }

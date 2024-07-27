9c2fb1ce29ebdbfa0e80eca44d0d1a1b44fd0ea7
Tom Marshall <tom.q.marshall@gmail.com>
2024-07-27 14:21:14 -0400

    send json as str

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -62,7 +62,7 @@ export class InfraStack extends Stack {
         ScheduleExpression: "rate(5 minutes)",
         Target: {
           Arn: lambdaFunction.functionArn,
-          Input: lambdaPayload,
+          Input: String(lambdaPayload),
           RoleArn: schedulerRole.roleArn,
         },
       },

3524dacd64aacdfe3dbd91536a4df385b9e1f6c7
Tom Marshall <tom.q.marshall@gmail.com>
2024-07-28 22:58:01 -0400

    debugging

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -104,5 +104,8 @@ export class InfraStack extends Stack {
 
   Tags.of(lambdaFunction).add("Customer", props.applicationTag);
 
+  const checkBucketName = Boolean(props.s3Bucket == "nitorilambda")
+  console.log("checkBucketName: '" + checkBucketName + "'")
+
   }
 }
